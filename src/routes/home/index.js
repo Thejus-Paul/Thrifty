dca49f0447c7b7d5fd13a880e00f20b8fc32f560
Thejus Paul <thejuspaul.me>
2020-10-19 16:38:20 +0530

    Added Navigation Bar

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,11 +2,13 @@ import { h } from 'preact';
 import style from './style';
 import MainBox from '../../components/MainBox/MainBox';
 import MiniStatements from '../../components/MiniStatements/MiniStatements';
+import Navigation from '../../components/Navigation/Navigation';
 
 const Home = () => (
 	<div class={style.home}>
 		<MainBox />
 		<MiniStatements />
+		<Navigation />
 	</div>
 );
 
