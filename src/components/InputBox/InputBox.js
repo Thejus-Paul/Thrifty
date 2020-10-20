ba22f89d104cf1fd28ab2761d9bb30f395a123d9
Thejus Paul <thejuspaul@pm.me>
2020-10-21 03:42:24 +0530

    Added InputBox Component

---
+++ src/components/InputBox/InputBox.js
@@ -0,0 +1,11 @@
+import { h, Component } from 'preact';
+import style from './style.css';
+
+const InputBox = (props) => (
+	<div class={style.container}>
+		<span class={style.name}>{props.name}</span>
+		<input type="text" class={style.input} />
+	</div>
+);
+
+export default InputBox;
--- src/routes/add_money/index.js
+++ src/routes/add_money/index.js
@@ -1,4 +1,5 @@
 import { h } from 'preact';
+import InputBox from '../../components/InputBox/InputBox';
 import style from './style';
 import { useState } from 'preact/hooks';
 
@@ -32,6 +33,10 @@ const AddMoney = () => {
 					<img id="cash" onClick={byCashPayment} alt="Cash icon" src="https://img.icons8.com/color/96/cash-.png"/>
 					<img id="card" class={style.inactive} onClick={byCardPayment} alt="Credit Card icon" src="https://img.icons8.com/color/96/bank-card-back-side.png"/>			
 				</div>
+			</div>
+			<div class={style.secondaryWindow}>
+				<InputBox name="Description" />
+				<InputBox name="Amount" />
 			</div>
 		</div>
 	);
