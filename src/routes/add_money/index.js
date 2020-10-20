695a04c652e0b96361952d9851de290f77b0c77f
Thejus Paul <thejuspaul@pm.me>
2020-10-21 03:18:24 +0530

    Added Add Window

--- src/routes/add_money/index.js
+++ src/routes/add_money/index.js
@@ -1,12 +1,40 @@
 import { h } from 'preact';
 import style from './style';
+import { useState } from 'preact/hooks';
 
-const AddMoney = () => (
-	<div class={style.home}>
-		<div class={style.MainBox}>
-			Add Money
+
+const inactive = (method) => { 
+	if(method === "card") {
+		document.getElementById("cash").removeAttribute("class");
+		document.getElementById("card").setAttribute("class",style.inactive);
+	} else {
+		document.getElementById("card").removeAttribute("class");
+		document.getElementById("cash").setAttribute("class",style.inactive);
+	}
+}
+
+const AddMoney = () => {
+	const [byCard, setByCard] = useState(false);
+	const byCardPayment = () => {
+		setByCard(true);
+		inactive("cash");
+	}
+	const byCashPayment = () => {
+		setByCard(false);
+		inactive("card");
+	}
+		
+	return(
+		<div class={style.home}>
+			<div class={style.container}>
+				Add Money to
+				<div class={style.options}>
+					<img id="cash" onClick={byCashPayment} alt="Cash icon" src="https://img.icons8.com/color/96/cash-.png"/>
+					<img id="card" class={style.inactive} onClick={byCardPayment} alt="Credit Card icon" src="https://img.icons8.com/color/96/bank-card-back-side.png"/>			
+				</div>
+			</div>
 		</div>
-	</div>
-);
+	);
+	}
 
 export default AddMoney;
