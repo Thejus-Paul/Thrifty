8958e5f131cc298ffaba9c3db1aa6edc9cd7a854
Thejus Paul <thejuspaul@pm.me>
2021-01-11 15:37:54 +0530

    feat(*): Adds Balance Operations

--- src/components/InputBox/index.js
+++ src/components/InputBox/index.js
@@ -1,11 +1,12 @@
 import { h } from 'preact';
 import style from './style.css';
 
-const InputBox = (props) => (
-	<div class={style.container}>
-		<span class={style.name}>{props.name}</span>
-		<input type="text" class={style.input} />
-	</div>
-);
+const InputBox = (props) => {
+	return(
+		<div class={style.container}>
+			<span class={style.name}>{props.name}</span>
+			<input type="text" class={style.input} onChange={props.value} />
+		</div>
+)};
 
 export default InputBox;
--- src/routes/add_money/index.js
+++ src/routes/add_money/index.js
@@ -1,32 +1,48 @@
 import { h } from 'preact';
-
-import { useState } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 import { Link } from 'preact-router/match';
+import Axios from 'axios';
 import InputBox from '../../components/InputBox';
 import Button from '../../components/Button';
 import style from './style';
 
-const inactive = (method) => { 
-	if(method === "card") {
-		document.getElementById("cash").removeAttribute("class");
-		document.getElementById("card").setAttribute("class",style.inactive);
-	} else {
-		document.getElementById("card").removeAttribute("class");
-		document.getElementById("cash").setAttribute("class",style.inactive);
-	}
-}
-
 const AddMoney = () => {
-	const [byCard, setByCard] = useState(false);
-	const byCardPayment = () => {
-		setByCard(true);
-		inactive("cash");
+	const [isCard, setCard] = useState(false);
+	const [desc, setDesc] = useState('');
+	const [money, setMoney] = useState(0);
+	// To change the payment method in UI
+	useEffect(() => {
+		if(isCard === false) {
+			document.getElementById("cash").removeAttribute("class");
+			document.getElementById("card").setAttribute("class",style.inactive);
+		} else {
+			document.getElementById("card").removeAttribute("class");
+			document.getElementById("cash").setAttribute("class",style.inactive);
+		}
+	},[isCard]);
+	// To change the payment method
+	const byCardPayment = () => setCard(true);
+	const byCashPayment = () => setCard(false);
+
+	const getDesc = (e) => setDesc(e.target.value);
+	const getMoney = (e) => setMoney(parseInt(e.target.value,10));
+	// To update balance
+	const addDetails = () => {
+		if(isNaN(money) === false && money !== 0) {
+			let data = {
+				byCard: isCard,
+				name: desc,
+				credit: false,
+				amount: money
+			}
+			Axios
+		  .post("http://localhost:3000/thrifty/balance", data)
+		  .then(res => console.log(res))
+		  .catch(err => console.log(err));
+		  window.location.href = '/';
+		}	  
 	}
-	const byCashPayment = () => {
-		setByCard(false);
-		inactive("card");
-	}
-		
+
 	return(
 		<div class={style.home}>
 			<Link href="/" class={style.goBack}>
@@ -43,12 +59,10 @@ const AddMoney = () => {
 			</div>
 			<div class={style.secondaryWindow}>
 				<div>
-					<InputBox name="Description" />
-					<InputBox name="Amount" />
+					<InputBox name="Description" value={getDesc} />
+					<InputBox name="Amount" value={getMoney} />
 				</div>
-				<Link href="/">
-					<Button name="ADD" />
-				</Link>
+				<Button name="ADD" action={addDetails} />
 			</div>
 		</div>
 	);
